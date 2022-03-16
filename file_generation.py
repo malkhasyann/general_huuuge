092a91d02e00362f25562500b8560029987bf18d
ASUS <ara.malkhasyan2021@gmail.com>
2022-03-16 15:32:09 +0400

    SOLVE

--- file_generation.py
+++ file_generation.py
@@ -1,10 +1,10 @@
-""" This script creates a text file of bilion integers. """
+""" This script creates a text file of billion integers. """
 import random
 
 MAX_RANDOM = 6_600_000  # max bound for random number generation
 
 with open('huuuge.txt', 'w') as f:
     i = 1
-    while i <= 7_000_000:  # amount of numbers
+    while i <= 50_000_000:  # amount of numbers
         f.write(f'{random.randint(0, MAX_RANDOM)}\n')
         i += 1
--- file_sorting.py
+++ file_sorting.py
@@ -1,57 +1,71 @@
 """ This script creates sorted copy of the file. """
 import os
+import shutil
 
-CURSORS = []  # keeping cursors of temporary files
-LAST_VALUES = []  # keeping last numbers of each temp file
+FILES = []  # keeping cursors of temp files
+SORTED_FILES = []  # keeping cursors of sorted temp files
+CURSOR_VALUES = []  # keeping current numbers of each cursor
 TEMP_SIZE = 2_000_000  # numbers count in each temp file
 
-dir_path = 'temp_dir'
+temp_path = 'temp_dir'
+sorted_temp_path = 'sorted_temp_dir'
 
-if not os.path.exists(dir_path):
-    os.makedirs(dir_path)
+# create directories for temp files and sorted temp files
+if not os.path.exists(temp_path):
+    os.makedirs(temp_path)
+if not os.path.exists(sorted_temp_path):
+    os.makedirs(sorted_temp_path)
 
-
-def min_index(arr: list):
-    """ Returns the index of minimum value in 'arr' """
-    if not arr:
-        return None
-
-    m = arr[0]  # min value
-    m_index = 0
-    for i in range(len(arr)):
-        if arr[i] < m:
-            m = arr[i]
-            m_index = i
-
-    return m_index
-
-
+# create temp files
 with open('huuuge.txt', 'r', encoding='utf-8') as file:
     i = 0
     for line in file:
-        if i % 2_000_000 == 0:
-            CURSORS.append(open(os.path.join(dir_path, f'file{i // TEMP_SIZE}.txt'), 'w+'))
-        CURSORS[-1].write(line)
+        if i % TEMP_SIZE == 0:
+            FILES.append(open(os.path.join(temp_path, f'file{i // TEMP_SIZE}.txt'), 'a+', encoding='utf-8'))
+        FILES[-1].write(line)
         i += 1
 
-LAST_VALUES = [CURSORS[i].readline() for i in range(len(CURSORS))]
+for file in FILES:  # put file cursor at the beginning of temp files
+    file.seek(0, 0)
+
+# create sorted temp files
+i = 0
+for file in FILES:
+    sorted_numbers = sorted(map(int, file))
+    SORTED_FILES.append(open(os.path.join(sorted_temp_path, f'sorted{i}.txt'), 'a+', encoding='utf-8'))
+    for number in sorted_numbers:
+        SORTED_FILES[-1].write(f'{number}\n')
+    i += 1
+
+for file in SORTED_FILES:  # put file cursor at the beginning of sorted temp files
+    file.seek(0, 0)
+
+# add first values of sorted temp files
+CURSOR_VALUES = [int(cursor.readline()) for cursor in SORTED_FILES]
 
 with open('sorted_huuuge.txt', 'w', encoding='utf-8') as file:
-    while LAST_VALUES:
-        current_index = min_index(LAST_VALUES)
-        if current_index is None:
-            break
-        file.write(f'{LAST_VALUES[current_index]}\n')
-        LAST_VALUES[current_index] = int(CURSORS[current_index].readline())
-        i = 0
-        while i < len(LAST_VALUES):
-            if LAST_VALUES[i] == '':
-                del LAST_VALUES[i]
-                CURSORS[i].close()
-                del CURSORS[i]
-            i += 1
+    while CURSOR_VALUES: # while CURSOR_VALUES is not empty
+        # find index of min value in CURSOR_VALUES
+        current_index = CURSOR_VALUES.index(min(CURSOR_VALUES))
 
+        file.write(f'{CURSOR_VALUES[current_index]}\n')
 
-# closing all opened temp files
-for file in CURSORS:
+        # read next value from file
+        next_value = SORTED_FILES[current_index].readline()
+        if len(next_value) != 0:  # if it is not the last line
+            # update value for the file
+            CURSOR_VALUES[current_index] = int(next_value)
+        else:
+            # if the next value is the last line
+            # delete the value from CURSOR_VALUES
+            # and delete corresponding file from SORTED_FILES
+            del CURSOR_VALUES[current_index]
+            SORTED_FILES[current_index].close()
+            del SORTED_FILES[current_index]
+
+for file in FILES:
     file.close()
+
+# delete temp files
+shutil.rmtree(temp_path)
+shutil.rmtree(sorted_temp_path)
